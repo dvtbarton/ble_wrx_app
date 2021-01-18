d5397252966aa7845fde1d423b3753368077e508
Todd Barton <dvtbarton@gmail.com>
2021-01-17 22:25:51 -0700

    added comment to clarify fillColorWheel()

--- lib/main.dart
+++ lib/main.dart
@@ -28,31 +28,37 @@ class ColorwheelApp extends StatelessWidget {
 }
 
 void fillColorWheel(List<Color> colors) {
+  //..............alpha  R  G  B
   // red to purple 0xFF FF 00 ↑↑
   for (var i = 1; i <= 0xff; i++) {
     colors.add(Color(0xFFFF0000 + i));
   }
 
+  //...............alpha  R  G  B
   // purple to blue 0xFF ↓↓ 00 FF
   for (var i = 1; i < 0xff; i++) {
     colors.add(Color(0xFFFF00FF - (i << 0x10)));
   }
 
+  //.............alpha  R  G  B
   // blue to cyan 0xFF 00 ↑↑ FF
   for (var i = 1; i < 0xff; i++) {
     colors.add(Color(0xFF0000FF + (i << 0x8)));
   }
 
+  //..............alpha  R  G  B
   // cyan to green 0xFF 00 FF ↓↓
   for (var i = 1; i <= 0xff; i++) {
     colors.add(Color(0xFF00FFFF - i));
   }
 
+  //................alpha  R  G  B
   // green to yellow 0xFF ↑↑ FF 00
   for (var i = 1; i < 0xff; i++) {
     colors.add(Color(0xFF00FF00 + (i << 0x10)));
   }
 
+  //..............alpha  R  G  B
   // yellow to red 0xFF FF ↓↓ 00
   for (var i = 1; i < 0xff; i++) {
     colors.add(Color(0xFFFFFF00 - (i << 0x8)));
